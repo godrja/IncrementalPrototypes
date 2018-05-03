7d070133efad714a9a8dad0bbf08b23a295576be
Konstantin Zmanovsky <konstantin.zmanovsky@gmail.com>
2018-05-03 23:09:18 +0200

    Experimenting with using reactstrap for UI

--- prototype_a/src/App.js
+++ prototype_a/src/App.js
@@ -2,17 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Button, Container, Row, Col } from 'reactstrap';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Incremental Prototype A</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
           Here is my first attempt to make a react application.
+          <br/>Hello!
+
+          <Container>
+            <Row>
+              <Col>
+                <Button color="primary">Click me</Button>{' '}
+              </Col>
+            </Row>
+          </Container>
         </p>
       </div>
     );
