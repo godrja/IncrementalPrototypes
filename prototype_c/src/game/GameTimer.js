1e113be1c64ffc6bdbd551c70e3feef439f925f5
Konstantin Zmanovsky <konstantin.zmanovsky@gmail.com>
2018-05-12 17:38:35 +0200

    Adding docs to GameTimer.js

--- prototype_c/src/game/GameTimer.js
+++ prototype_c/src/game/GameTimer.js
@@ -1,4 +1,10 @@
-const startTicking = (callback, interval = 100) => {
+/**
+ * Start timer that will generate a tick every interval milliseconds
+ * @param {function} callback function to call every tick
+ * @param {number} interval Interval in milliseconds
+ * @returns {{startTicking: startTicking, stopTicking: stopTicking}}
+ */
+function startTicking(callback, interval = 100) {
   const intervalId = setInterval(() => {
     // TODO: Make sure that if the interval is longer than (interval * 2) - 1 then multiple tick events get triggered
     callback();
@@ -9,6 +15,6 @@ const startTicking = (callback, interval = 100) => {
       clearInterval(intervalId);
     }
   }
-};
+}
 
 export default { startTicking }
